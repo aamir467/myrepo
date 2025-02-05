73aec08211986736ce4c4b6400d611f3b52b46e4
Aamir Shamim <aamirshamimabc@gmail.com>
2025-02-05 20:26:11 +0530

    product of two number

--- main.cpp
+++ main.cpp
@@ -4,12 +4,15 @@ int main(){
 	cout<<"Hello Ashar"<<endl;
 	cout<<"Hello Everyone"<<endl;
 	// add two number 
-	int x,y,z;
+	int x,y,sum,product;
 	cout<<"Enter  the number :\t";
 	cin>>x;
 	cout<<"Enter the number :\t";
 	cin>>y;
-	z=x+y;
-	cout<<"sum of two number is :\t"<<z<<endl;
+	sum=x+y;
+	product=x*y;
+	cout<<"sum of two number is :\t"<<sum<<endl;
+	cout<<"Product of two number is :\t"<<product<<endl;
+	
 }
 
