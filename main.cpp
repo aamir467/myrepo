ef2985586162b9c9e8f958d5b446a6ff197b1dc1
Aamir Shamim <aamirshamimabc@gmail.com>
2025-02-05 19:58:24 +0530

    multiply two number

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-	cout<<"Hello Ashar";
-	cout<<"Hello Everyone";
+	cout<<"Hello Ashar"<<endl; 
+	cout<<"Hello Everyone"<<endl;
+	// multiply two number 
+	int x,y,z;
+	cout<<"Enter  the number :\t";
+	cin>>x;
+	cout<<"Enter the number :\t";
+	cin>>y;
+	z=x*y;
+	cout<<"sum of two number is :\t"<<z<<endl;
 }
+
